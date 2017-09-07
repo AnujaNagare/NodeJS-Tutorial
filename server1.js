e876386efd7a0708a53fe996017ceda3c0586ffb
Anuja Nagare <AnujaNagare@users.noreply.github.com>
2017-09-07 18:39:58 -0400

    Add files via upload

---
+++ server1.js
@@ -0,0 +1,23 @@
+// simple web server
+var http = require('http');
+var connect = require('connect');
+
+var app = connect();
+
+function doFirst(request, respose, next){
+	console.log("Hello !")
+	next();
+}
+
+function doSecond(request, respose, next){
+	console.log("Bye !")
+
+}
+
+app.use(doFirst);
+app.use(doSecond);
+
+
+http.createServer(app).listen(8000);
+console.log("Server is now running.....")
+
