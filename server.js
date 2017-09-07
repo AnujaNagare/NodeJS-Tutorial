26a6f480a1f991d44bb00d5bef0e774ddd4a68bc
Anuja Nagare <AnujaNagare@users.noreply.github.com>
2017-09-07 18:26:12 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
-// basic server setup program
+// simple web server
 var http = require('http');
+var fs = require('fs');
 
-// onRequest function
-function onRequest(request, response){
-	console.log("A user made a request" + request.url);
-	// 200 implies that the response contains a payload that represents the status of the requested resource
-	response.writeHead(200, {"Context-Type": "text/plain"});
-	response.write("Here is your data");
+// 404 response
+function send404Response(response){
+	
+	response.writeHead(404, {"Content-Type": "text/plain"});
+	response.write("Error 404: Page not found");
 	response.end()
 }
+
+// handle a user request 
+function onRequest(request, response){
+	if (request.method = 'GET' && request.url == '/'){
+		response.writeHead(200, {"Content-Type": "text/html"});
+		fs.createReadStream("./index.html").pipe(response);
+		// response.end()
+	}
+	else
+	{
+		send404Response(response);
+	}
+
+}
+
 
 http.createServer(onRequest).listen(8000);
 console.log("Server is now running.....")
